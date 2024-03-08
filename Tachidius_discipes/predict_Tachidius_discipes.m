d3d04773fa0f17dd988ac64fb2ba3cac89498895
Juvigliano <julieta.vigliano@ugent.be>
2024-03-08 12:03:58 +0100

    summary

--- Tachidius_discipes/predict_Tachidius_discipes.m
+++ Tachidius_discipes/predict_Tachidius_discipes.m
@@ -15,8 +15,8 @@ function [prdData, info] = predict_Tachidius_discipes(par, data, auxData)
   
   % life cycle
   % life cycle
-  pars_tjj = [g k l_T v_Hb v_Hj];
-  pars_tjp = [g k l_T v_Hb v_Hp];
+  pars_tjj = [g k l_T v_Hb v_Hj v_Hp];
+  pars_tjp = [g k l_T v_Hb v_Hp v_Hp];
   
   % gets scaled age and length at birth and metam
   [t_j, ~, t_b, l_j, ~, l_b, ~, ~, ~, info_j] = get_tj(pars_tjj, f);
@@ -50,7 +50,7 @@ function [prdData, info] = predict_Tachidius_discipes(par, data, auxData)
   
    
   % pack to output
-  prdData.am  = aT_m;            % d, life span
+  % prdData.am  = aT_m;            % d, life span
   prdData.ah  = (t_0 + a_b)/ TC; % d, age at hatch
   prdData.tp  = aT_p - aT_j;     % d, time since metam at puberty
   prdData.Lh  = Lw_b;            % cm, length at hatch (assumed to equal length at birth)
@@ -65,7 +65,7 @@ function [prdData, info] = predict_Tachidius_discipes(par, data, auxData)
   L_b  = L_m * l_b;                                     % cm, structural length at birth at f = f_tL
   L_p  = L_m * l_p;                                     % cm, structural length at puberty at f = f_tL
   rT_j = TC * v * (f_tL/ L_b - 1/ L_m)/ (f_tL + g);     % 1/d, specific growth rate during accelleration at f = f_tL
-  ELw  = min(L_p, L_b * exp(tL(:,1) * rT_j/ 3))/ del_M; % cm, length at f = f_tL
+  ELw  = min(L_p, L_b * exp(f_tL(:,1) * rT_j/ 3))/ del_M; % cm, length at f = f_tL
   
   
   % temperature-development time at f = f_Tt
@@ -83,9 +83,9 @@ function [prdData, info] = predict_Tachidius_discipes(par, data, auxData)
   
   % pack to output
   prdData.tL  = ELw;           % d, time        - cm, length
-  prdData.Xtj = aT_jX - aT_bX; % mug C/mL, food - d, time since hatch at metam
-  prdData.Xtp = aT_pX - aT_jX; % mug C/mL, food - d, time since metam at puberty
-  prdData.XR  = RT_iX;         % mug C/mL, food - #/d, reprod rate
+  % prdData.Xtj = aT_jX - aT_bX; % mug C/mL, food - d, time since hatch at metam
+  % prdData.Xtp = aT_pX - aT_jX; % mug C/mL, food - d, time since metam at puberty
+  % prdData.XR  = RT_iX;         % mug C/mL, food - #/d, reprod rate
   prdData.Tah = Ea_b;          % C, temperature - d, inter-brood time
   prdData.Ttj = Et_j;          % C, temperature - d, time since hatch at metam
   prdData.Ttp = Et_p;          % C, temperature - d, time since metam at puberty
